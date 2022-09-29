6702f1aff7afe797c6230f8188a17e9cfdaf9722
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-09-29 16:40:54 +0300

    Update task8.c

--- Lab1/task8.c
+++ Lab1/task8.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL, "Russian");
     int mas1[128], new_mas[64];
     int k = 0, len = 0, sum = 0, len1 = 0, len2 = 0;
     int max = 0, min = 10000000;
@@ -12,19 +14,19 @@ int main(){
         perror("Can't open file");
         return 1;
     }
-    
+
     while (fscanf(fi, "%d", mas1 + len) != EOF)
         len++;
-    
+
     printf("Меню:\n");
     printf("1.Переписать числа, стоящие на нечетных позициях\n");
     printf("2.Переписать четные числа\n");
     printf("3.Для текущего элемента: наиболее далёкое (по значению) от него значение из этого же массива\n");
     printf("4.Для текущего элемента: сумму элементов, которые ему предшествуют\n");
     printf("5.Для текущего элемента: сумму элементов, которые меньше него\n");
-    
+
     scanf("%d", &action);
-    
+
     switch(action){
         case 1:
             for (int i = 0; i < len / 2; i++){
@@ -32,7 +34,7 @@ int main(){
             	printf("%d ", new_mas[i]);
         }
         break;
-        
+
         case 2:
             for (int i = 0; i < len; i++){
                 if (mas1[i] % 2 == 0){
@@ -42,7 +44,7 @@ int main(){
                 }
             }
         break;
-        
+
         case 3:
             printf("Введите индекс текущего элемента:\n");
             scanf("%d", &indx);
@@ -54,22 +56,22 @@ int main(){
             		min = mas1[i];
             	}
             }
-         
+
             if (mas1[indx] == max){
             	new_mas[k] = min;
             	printf("%d", new_mas[k]);
             	break;
             }
-            
+
             if (mas1[indx] == min){
             	new_mas[k] = max;
             	printf("%d", new_mas[k]);
             	break;
             }
-            
+
             len1 = max - mas1[indx];
             len2 = mas1[indx] - min;
-            
+
             if (len1 > len2){
             	new_mas[k] = max;
             	printf("%d", new_mas[k]);
@@ -78,20 +80,20 @@ int main(){
             	new_mas[k] = min;
             	printf("%d", new_mas[k]);
             }
-            
+
             break;
-            
+
         case 4:
             printf("Введите индекс текущего элемента:\n");
             scanf("%d", &indx);
             for (int i = 0; i < indx; i++){
                 sum += mas1[i];
             }
-            
+
             new_mas[k] = sum;
             printf("%d", new_mas[k]);
             break;
-            
+
         case 5:
             printf("Введите индекс текущего элемента:\n");
             scanf("%d", &indx);
@@ -100,11 +102,11 @@ int main(){
                     sum += mas1[i];
                 }
             }
-            
+
             new_mas[k] = sum;
             printf("%d", new_mas[k]);
             break;
-            
+
         default:
             printf("Неверный пункт меню!");
             break;
