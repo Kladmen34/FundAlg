24974f51c65b8fc69370fcd9441147041b676281
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-10-04 14:29:29 +0300

    Update task8.c

--- Lab2/task8.c
+++ Lab2/task8.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <locale.h>
 
 double f(double x){
     return 2 * log(x) - sin(x) + 2 * pow(x, 2);
@@ -24,7 +25,13 @@ double Dhtm(double a, double b, double epsylone){
 }
 
 int main(){
-    double eps = 0.0000001;
-    printf("x = %lf\n", Dhtm(0, 1, eps));
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    double eps;
+    printf("Введите границы отрезка: ");
+    scanf("%d %d", &a, &b);
+    printf("\nВведите точность, с которой необходимо вычислить корень: ");
+    scanf("%lf", &eps);
+    printf("x = %lf\n", Dhtm(a, b, eps));
     return 0;
 }
