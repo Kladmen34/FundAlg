f9be89b83c253bae370af74120b4e3514027d6ea
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-10-12 14:44:59 +0300

    Add files via upload

---
+++ Lab1/task4.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAXSTRING 100
+int main(){
+  FILE *f1, *f2;
+  char lex1[MAXSTRING], lex2[MAXSTRING], lex3[MAXSTRING];
+  f1 = fopen("in.txt","r");
+  f2 = fopen("out.txt","w");
+
+  if (!f1){
+    perror("Can't open file");
+    return 1;
+  }
+
+  if (!f2){
+    perror("Can't open file");
+    return 2;
+  }
+
+  while (!feof(f1)){
+    fscanf(f1, "%s%s%s", lex1,lex2,lex3);
+    fprintf(f2, "%s %s %s%s", lex3,lex1,lex2,"\n");
+  }
+  fclose(f1);
+  fclose(f2);
+
+  f1 = fopen("in.txt","w");
+  f2 = fopen("out.txt","r");
+
+  if (!f1){
+    perror("Can't open file");
+    return 3;
+  }
+
+  if (!f2){
+    perror("Can't open file");
+    return 4;
+  }
+
+  char c;
+  while ((c = fgetc(f2)) != EOF)
+    fputc(c, f1);
+  fclose(f2);
+  fclose(f1);
+  f2 = fopen("out.txt","w");
+  fclose(f2);
+  return 0;
+}
