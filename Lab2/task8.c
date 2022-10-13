2b0364d9fb50dbe20befa1c7d4ce38afad239327
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-10-13 18:01:18 +0300

    Update task8.c

--- Lab2/task8.c
+++ Lab2/task8.c
@@ -6,28 +6,26 @@ double f(double x){
     return 2 * log(x) - sin(x) + 2 * pow(x, 2);
 }
 
-double Dhtm(double a, double b, double epsylone){
+double Dhtm(double a, double b, int k){
+    double epsylone = pow(10, -k);
     double c;
-    if (f(a) * f(b) < 0){
-        while (fabs(b - a) > epsylone){
-            c = (a + b) / 2;
-            if (f(c) * f(a) > 0)
-                a = c;
-            else
-                b = c;
-        }
-        return c;
+    while (fabs(b - a) > epsylone){
+        c = (a + b) / 2;
+        if (f(c) * f(a) > 0)
+            a = c;
+        else
+            b = c;
     }
+    return c;
 }
 
 int main(){
     setlocale(LC_ALL, "Russian");
-    int a, b;
-    double eps;
+    int a, b, k;
     printf("Введите границы отрезка: ");
     scanf("%d %d", &a, &b);
     printf("\nВведите точность, с которой необходимо вычислить корень: ");
-    scanf("%lf", &eps);
-    printf("x = %lf\n", Dhtm(a, b, eps));
+    scanf("%d", &k);
+    printf("x = %lf\n", Dhtm(a, b, k));
     return 0;
 }
