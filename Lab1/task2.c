3cd3cb06638dbcb20fe418d76d4ea18357fae873
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-11-08 22:43:09 +0300

    Update task2.c

--- Lab1/task2.c
+++ Lab1/task2.c
@@ -18,27 +18,50 @@ int char_to_int(char* string) {
 	return result * sign;
 }
 
-double square_equation(double a, double b, double c) {
+double char_to_double(char* string) {
+	int sign = 1, sm = 0, point_pos = 0;
+	double backp = 0, free = 1;
+	if (*string == '-') {
+		sm++;
+		sign = -1;
+	}
+	for (int i = sm; string[i] != '\0'; i++) {
+		if (point_pos) {
+			free = free / 10;
+			backp = backp + free * (string[i] - '0');
+		}
+		else {
+			if (string[i] == '.') {
+				point_pos = 1;
+			}
+			else {
+				backp = backp * 10 + (string[i] - '0');
+			}
+		}
+	}
+	return sign * backp;
+}
+
+double square_equation(double a, double b, double c, double* x1, double* x2) {
 	double eps = 0.0000001;
 	if (fabs(a - 0) < eps) {
 		printf("Неверный ввод!\n");
 		return 1;
 	}
-	float D;
-	float x1, x2;
+	double D;
 	D = pow(b, 2) - (4 * a * c);
-	printf("При a = %d, b = %d, c = %d:\n", a, b, c);
-	if (D < eps) {
+	printf("При a = %lf, b = %lf, c = %lf:\n", a, b, c);
+	if (D < -eps) {
 		printf("Вещественных решений нет\n");
 		return -1;
 	}
 	else {
-		x1 = (-b + sqrt(D)) / ((2 * a));
-		x2 = (-b - sqrt(D)) / ((2 * a));
-		printf("Корни - %f %f\n", x1, x2);
+		*x1 = (-b + sqrt(D)) / ((2 * a));
+		*x2 = (-b - sqrt(D)) / ((2 * a));
+		printf("Корни - %lf %lf\n", *x1, *x2);
 		return 0;
 	}
-	
+
 }
 
 
@@ -48,8 +71,9 @@ int main(int argc, char* argv[]) {
 	if (strlen(argv[1]) != 2) {
 		printf("Неверная длина флага!\n");
 	}
-	int a, b, c;
+	double a, b, c;
 	int num1, num2, num3;
+	double x1, x2;
 	if (argv[1][0] == '-' || argv[1][0] == '/') {
 		switch (argv[1][1]) {
 		case 'q':
@@ -57,17 +81,17 @@ int main(int argc, char* argv[]) {
 				printf("Неверное число параметров!\n");
 				break;
 			}
-			a = char_to_int(argv[2]);
-			b = char_to_int(argv[3]);
-			c = char_to_int(argv[4]);
-			square_equation(a, b, c);
-			square_equation(a, c, b);
-			square_equation(b, a, c);
-			square_equation(b, c, a);
-			square_equation(c, a, b);
-			square_equation(c, b, a);
+			a = char_to_double(argv[2]);
+			b = char_to_double(argv[3]);
+			c = char_to_double(argv[4]);
+			square_equation(a, b, c, &x1, &x2);
+			square_equation(a, c, b, &x1, &x2);
+			square_equation(b, a, c, &x1, &x2);
+			square_equation(b, c, a, &x1, &x2);
+			square_equation(c, a, b, &x1, &x2);
+			square_equation(c, b, a, &x1, &x2);
 			break;
-			
+
 		case 'm':
 			if (argc != 4) {
 				printf("Неверное число параметров!\n");
@@ -87,7 +111,7 @@ int main(int argc, char* argv[]) {
 			else {
 				printf("Некорректный ввод!\n");
 			}
-			
+
 			break;
 		case 't':
 
@@ -98,11 +122,11 @@ int main(int argc, char* argv[]) {
 			num1 = char_to_int(argv[2]);
 			num2 = char_to_int(argv[3]);
 			num3 = char_to_int(argv[4]);
-				
+
 			if (num1 > 0 && num2 > 0 && num3 > 0) {
-			num1 = pow(num1, 2);
-			num2 = pow(num2, 2);
-			num3 = pow(num3, 2);
+				num1 *= num1;
+				num2 *= num2;
+				num3 *= num3;
 				if (num1 == num2 + num3 || num2 == num1 + num3 || num3 == num1 + num2) {
 					printf("Это стороны прямоугольного треугольника\n");
 				}
@@ -114,7 +138,7 @@ int main(int argc, char* argv[]) {
 				printf("Некорректный ввод!\n");
 			}
 			break;
-		}	
+		}
 	}
 	return 0;
 }
