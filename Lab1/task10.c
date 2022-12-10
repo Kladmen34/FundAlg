cebc64ef037ad2f7a605681f96d3e24704ff6bd7
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-12-10 12:55:33 +0300

    Update task10.c

--- Lab1/task10.c
+++ Lab1/task10.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void filling_matrix(double **matrix, int rowscount, int columnscount){
-    for (int i = 0; i < rowscount; i++){
-        for (int j = 0; j < columnscount; j++){
+#define EPS 0.0000001
+
+double **generate_matrix(int *rows, int *columns) {
+    *rows = rand() % 10 + 1;
+    *columns = rand() % 10 + 1;
+    double **matrix = (double**)malloc(sizeof(double*) * *rows);
+    if(matrix == NULL) {
+        return NULL;
+    }
+
+    for(int i =0; i < *rows; i++) {
+        matrix[i] = (double*)malloc(sizeof(double) * *columns);
+        if(matrix[i] == NULL) {
+            return NULL;
+        }
+        for(int j=0; j < *columns; j++) {
             matrix[i][j] = rand() % 201 - 100;
         }
     }
+    return matrix;
 }
 
 void print_matrix(double **matrix, int rowscount, int columnscount){
@@ -18,101 +32,120 @@ void print_matrix(double **matrix, int rowscount, int columnscount){
     }
 }
 
-double **mult(double **matrix1, double **matrix2, int l, int m, int n) {
-    double **result =(double**)malloc(l * sizeof(double*));
-    for (int M = 0; M < l; M++){
-        result[M] = (double*)malloc(n * sizeof(double));
-    }
-    for (int i = 0; i < l; i++){
-        for (int j = 0; j < n; j++){
-            result[i][j] = 0;
-            for (int r = 0; r < m; r++){
-                result[i][j] += matrix1[i][r] * matrix2[r][j];
+double **matr_mult(double **matrix1, double **matrix2, int rows_m1, int columns_m1, int rows_m2, int columns_m2) {
+    double **new_matrix = NULL;
+    if(columns_m1 == rows_m2) {
+        new_matrix = (double**)malloc(sizeof(double*) * rows_m1);
+        if(new_matrix == NULL) {
+            return NULL;
+        }
+        for(int i = 0; i < rows_m1; i++) {
+            new_matrix[i] = (double*)malloc(sizeof(double) * columns_m2);
+            if(new_matrix[i] == NULL) {
+                return NULL;
+            }
+        }
+
+        for(int i = 0; i < rows_m1; i++) {
+            for(int j = 0; j < columns_m2; j++) {
+                new_matrix[i][j] = 0;
+                for(int k = 0; k < columns_m1; k++) {
+                    new_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
             }
         }
     }
-    return result;
+    return new_matrix;
 }
 
-double determinant(double **a, int n){
-    int i = 0, j = 0, k = 0, l = 0;
-    double det;
-    while (k < n - 1){
-        while (i < n){
-            det = a[i][j] / a[k][l];
-            while (j < n){
-                a[i][j] += (-det) * a[k][j];
-                j++;
+
+int determinant(double **matrix, int rows, int columns) {
+    if(rows != columns) {
+        return -1;
+    } else {
+        int det = 1;
+        double* tmp;
+        for (int i = 0; i < rows; i++) {
+            int k = i;
+            for (int j = i + 1; j < rows; j++)
+                if (fabs(matrix[j][i]) > fabs(matrix[k][i]))
+                    k = j;
+            if (fabs(matrix[k][i]) < EPS) {
+                det = 0;
+                break;
             }
-            j = l;
-            i++;
+            tmp = matrix[i];
+            matrix[i] = matrix[k];
+            matrix[k] = tmp;
+            if (i != k)
+                det = -det;
+            det *= matrix[i][i];
+            for (int j = i + 1; j < rows; j++)
+                matrix[i][j] /= matrix[i][i];
+            for (int j = 0; j < rows; ++j)
+                if (j != i && fabs(matrix[j][i]) > EPS)
+                    for (int k = i + 1; k < rows; k++)
+                        matrix[j][k] -= matrix[i][k] * matrix[j][i];
         }
-        k++;
-        l++;
-        i = k + 1;
-        j = l;
+        return det;
     }
-    det = 1;
-    i = 0;
-    while (i < n){
-        det = a[i][i] * det;
-        i++;
-    }
-    return det;
 }
 
-void matrix_cleaning(double **a, int rowscount){
+
+void matrix_cleaning(double ***a, int rowscount){
     for (int i = 0; i < rowscount; i++)
         free(a[i]);
     free(a);
+    a = NULL;
 }
 
 int main(){
-    int n;
-    double **matrix1, **matrix2, **matr_mult;
-    int rowscount1 = rand() % 10 + 1;
-    int columnscount1 = rand() % 10 + 1;
-    int rowscount2 = rand() % 10 + 1;
-    int columnscount2 = rand() % 10 + 1;
-    matrix1 = (double**)malloc(rowscount1 * sizeof(double*));
-    for (int i = 0; i < rowscount1; i++)
-        matrix1[i] = (double*)malloc(columnscount1 * sizeof(double));
-    filling_matrix(matrix1, rowscount1, columnscount1);
-    printf("Matrix 1:\n");
-    print_matrix(matrix1, rowscount1, columnscount1);
+    int rows_of_matrix1 = 0, columns_of_matrix1 = 0;
+    int rows_of_matrix2 = 0, columns_of_matrix2 = 0;
+    int determinant_of_matrix1, determinant_of_matrix2;
+    double **matrix1 = generate_matrix(&rows_of_matrix1, &columns_of_matrix1);
 
-    matrix2 = (double**)malloc(rowscount2 * sizeof(double*));
-    for (int i = 0; i < rowscount2; i++)
-        matrix2[i] = (double*)malloc(columnscount2 * sizeof(double));
-    filling_matrix(matrix2, rowscount2, columnscount2);
-    printf("Matrix 2:\n");
-    print_matrix(matrix2, rowscount2, columnscount2);
-
-    if (columnscount1 == rowscount2){
-        matr_mult = mult(matrix1, matrix2, rowscount1, columnscount2, columnscount1);
-        printf("Matrix 1 * 2:\n");
-        print_matrix(matr_mult, rowscount1, columnscount2);
-        matrix_cleaning(matr_mult, rowscount1);
+    if(matrix1 != NULL) {
+        printf("First matrix: \n");
+        print_matrix(matrix1, rows_of_matrix1, columns_of_matrix1);
+    } else {
+        printf("Failed to generate 1 matrix");
     }
-    else
-        printf("Operation is not possible\n");
 
-
-    if (rowscount1 == columnscount1){
-        n = rowscount1 = columnscount1;
-        printf("Determinant of matrix1 = %1.1f\n", determinant(matrix1, n));
+    double **matrix2 = generate_matrix(&rows_of_matrix2, &columns_of_matrix2);
+    if(matrix1 != NULL) {
+        printf("Second matrix: \n");
+        print_matrix(matrix2, rows_of_matrix2, columns_of_matrix2);
+    } else {
+        printf("Failed to generate 2 matrix");
     }
-    else
-        printf("Operation is not possible\n");
 
-    if (rowscount2 == columnscount2){
-        n = rowscount2 = columnscount2;
-        printf("Determinant of matrix2 = %1.1f\n", determinant(matrix2, n));
+    double **new_matrix = matr_mult(matrix1, matrix2, rows_of_matrix1, columns_of_matrix1, rows_of_matrix2, columns_of_matrix2);
+    if(new_matrix != NULL) {
+        printf("After multiplying 2 matrices: \n");
+        print_matrix(new_matrix, rows_of_matrix1, columns_of_matrix2);
+        matrix_cleaning(&new_matrix, rows_of_matrix1);
+    } else {
+        printf("Failed to multiply 2 matrices\n");
     }
-    else
-        printf("Operation is not possible\n");
 
-    matrix_cleaning(matrix1, rowscount1);
-    matrix_cleaning(matrix2, rowscount2);
+    determinant_of_matrix1 = determinant(matrix1, rows_of_matrix1, columns_of_matrix1);
+    determinant_of_matrix2 = determinant(matrix2, rows_of_matrix2, columns_of_matrix2);
+
+    if(determinant_of_matrix1 != -1) {
+        printf("Determinant of first matrix: %d\n", determinant_of_matrix1);
+    } else {
+        printf("Failed to find determinant of first matrix\n");
+    }
+
+    if(determinant_of_matrix2 != -1) {
+        printf("Determinant of second matrix: %d\n", determinant_of_matrix2);
+    } else {
+        printf("Failed to find determinant of second matrix\n");
+    }
+
+    matrix_cleaning(&matrix1, rows_of_matrix1);
+    matrix_cleaning(&matrix2, rows_of_matrix2);
+
     return 0;
 }
