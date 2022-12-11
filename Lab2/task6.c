6173b80ab503335009cf6de7771476cffd7d10bd
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2022-12-11 16:56:31 +0300

    Update task6.c

--- Lab2/task6.c
+++ Lab2/task6.c
@@ -1,150 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
+#include <stdarg.h>
 
-typedef struct found {
-    char* filename;
-    int count;
-    int* string_numbers;
-} found;
 
-int finding_substring(char* str, char* substr)
-{
-    int len = strlen(substr);
-    char* ref = substr;
-    while (*str && *ref)
-    {
-        if (*str++ == *ref)
-        {
-            ref++;
-        }
-        if (!*ref)
-        {
-            return (1);
-        }
-        if (len == (ref - substr))
-        {
-            ref = substr;
-        }
+enum {
+    FILE_NOT_FOUND = -1,
+    SUBSTR_FOUND = -2,
+    SUBSTR_NOT_FOUND = -3,
+    MEMORY_ERROR = -4,
+    SUCCESSFULLY = -5
+};
+
+int find_str(char *str, char *substr) {
+    int i, j, str_len, substr_len;
+	str_len = strlen(str);
+	substr_len = strlen(substr);
+	int *prefix = (int*)malloc(substr_len * sizeof(int));
+    if (!prefix){
+        free(*prefix);
+        return MEMORY_ERROR;
     }
-    return 0;
+
+	prefix[0] = 0;
+	for(i = 1, j = 0; i < substr_len; i++) {
+		while(j > 0 && substr[j] != substr[i])
+			j = prefix[j-1];
+		if(substr[j] == substr[i])
+			j++;
+		prefix[i] = j;
+	}
+
+
+	for(i = 0, j = 0; i < str_len; i++){
+		while(j > 0 && substr[j] != str[i])
+			j = prefix[j - 1];
+		if(substr[j] == str[i]) {
+            j++;
+        }
+		if(j == substr_len) {
+		    free(prefix);
+            return SUBSTR_FOUND;
+        }
+	}
+    free(prefix);
+	return SUBSTR_NOT_FOUND;
 }
 
-found* search(char* substr, int num, ...)
-{
-    found *res = (found*)malloc(num * sizeof(found));
-    if (res == NULL)
-        return NULL;
-    int k = 0, filename_length=0;
-    va_list args;
-    char* str[BUFSIZ];
-    FILE* f;
-    char* filename;
-    va_start(args, num);
-    filename = va_arg(args, char*);
-    for(int i=0; i<num;i++)
-    {
-        filename_length = strlen(filename);
-        res[i].filename = (char*)malloc(sizeof(char) * filename_length);
-        if (res[i].filename == NULL)
-        {
-            for (int l = 0; l < i; l++)
-            {
-                free(res[l].string_numbers);
-                free(res[l].filename);
-            }
-            return NULL;
+
+int check_files(char *substr, int num_of_files, ...) {
+    FILE *fin;
+    char *file_name;
+    va_list p;
+    va_start(p, num_of_files);
+    int is_found = 0, line = 1;
+    char str[BUFSIZ];
+    while(num_of_files != 0) {
+        line = 1;
+        num_of_files--;
+        file_name = va_arg(p, char*);
+        fin = fopen(file_name, "r");
+        if(!fin) {
+            return FILE_NOT_FOUND;
         }
-        res[i].count = 1;
-        res[i].string_numbers = (int*)malloc(sizeof(int) * res[i].count);
-        if (res[i].string_numbers == NULL)
-        {
-            for (int l = 0; l < i; l++)
-            {
-                free(res[l].string_numbers);
-                free(res[l].filename);
+        while(fgets(str, sizeof(str), fin) != NULL) {
+            is_found = find_str(str, substr);
+            if(is_found == SUBSTR_FOUND) {
+                printf("Substr %s found in file: %s, in line=%d\n", substr, file_name, line);
+            } else if(is_found == SUBSTR_NOT_FOUND) {
+                printf("Substr %s NOT found in file: %s, in line=%d\n", substr, file_name, line);
+            } else if(is_found == MEMORY_ERROR) {
+                printf("Problems with memory...\n");
+                return MEMORY_ERROR;
             }
-            free(res[i].filename);
-            return NULL;
+            line++;
         }
-        if ((f = fopen(filename, "r")) != NULL)
-        {
-            strcpy(res[i].filename, filename);
-            int line = 1;
-            while (fgets(str, sizeof(str), f) != NULL)
-            {
-                while (1)
-                {
-                    if (finding_substring(str, substr) != 0)
-                    {
-                        printf("Substring %s found in string number %d\n", substr, line);
-                        res[i].count++;
-                        res[i].string_numbers[k++] = line;
-                        if (k == res[i].count)
-                        {
-                            res[i].count += 1;
-                            res[i].string_numbers = (int*)realloc(res[i].string_numbers, res[i].count * sizeof(int));
-                            if (res[i].string_numbers == NULL)
-                            {
-                                for (int l = 0; l < i; l++)
-                                {
-                                    free(res[l].string_numbers);
-                                    free(res[l].filename);
-                                }
-                                free(res[i].filename);
-                                return NULL;
-                            }
-                        }
-                            
-                    }
-                    break;
-                }
-                line++;
-            }
-            fclose(f);
-            k = 0;
-            filename = va_arg(args, char*);
-        }
+        fclose(fin);
     }
-    va_end(args);
-    return res;
+    va_end(p);
+    return SUCCESSFULLY;
 }
 
-void rfree(int num, found* res)
-{
-    for (int i = 0; i < num; i++)
-    {
-        free((res+i)->filename);
-        free((res+i)->string_numbers);
-    }
-    
-}
-
-int main()
-{
-    int num = 2;
-    found *res ;
-    if ((res = search("Hello", num, "f61.txt", "f62.txt")) != NULL)
-    {
-        for (int i = 0; i < num; i++)
-        {
-            printf("%s\n", res[i].filename);
-            if (res[i].count > 1)
-                for (int j = 0; j < res[i].count-1; j++)
-                {
-                    printf("%d\n", res[i].string_numbers[j]);
-                }
-            else
-                printf("No matches\n");
-        }
-        rfree(num, res);
-        free(res);
-    }
-    else
-    {
-        printf("Error during search\n");
-        free(res);
-    }
-    return 0;
+int main(int argc, char * argv[]) {
+    check_files("Hello", 2, "f1.txt","f2.txt");
 }
