d2ed9ec54cf3bce86bde0fa107646424d786a8d6
Aleksandr Babenko <113621222+Kladmen34@users.noreply.github.com>
2023-04-22 23:32:52 +0300

    Update memory_concrete.cpp

--- Lab5/task2/memory_concrete.cpp
+++ Lab5/task2/memory_concrete.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "memory_concrete.h"
 #include <sstream>
-
+#include <cstring>
 
 void memory_concrete::log(std::string const& target, logger::severity level) const {
 	if (_log == nullptr)
@@ -15,29 +15,31 @@ memory_concrete::memory_concrete(logger* log) {
 
 void* memory_concrete::allocate(size_t target_size) const {
 	void* ptr = nullptr;
-	try { ptr = ::operator new(target_size); }
+	try { ptr = ::operator new(sizeof(size_t) + target_size); }
 	catch (std::bad_alloc) {
 		log("Memory allocated error!", logger::error);
 		throw;
 	}
+	*reinterpret_cast<size_t*>(ptr) = target_size;
 	std::ostringstream stream;
-	stream << "Allocated memory with adress " << ptr;
+	stream << "Allocated memory with address " << ptr;
 	log(stream.str(), logger::information);
-	return ptr;
+	return reinterpret_cast<void *>(reinterpret_cast<size_t *>(ptr) + 1);
 }
 
 void memory_concrete::deallocate(void const* const target_to_dealloc) const {
-	unsigned char const* mem = static_cast<unsigned char const*>(target_to_dealloc);
+	unsigned char* mem = reinterpret_cast<unsigned char*>(const_cast<void *>(target_to_dealloc));
+	size_t target_size = *(mem - sizeof(size_t));
 
 	std::ostringstream log_stream;
 
 	log_stream << "Memory deallocated at " << target_to_dealloc << " with contents: ";
-
-	for (size_t i = 0; i < 65; ++i) {
-		log_stream << static_cast<int>(mem[i]) << " ";
+	
+	for (size_t i = 0; i < target_size; ++i) {
+		log_stream << static_cast<unsigned short>(mem[i]) << " ";
 	}
 	
 	log(log_stream.str(), logger::information);
-	::operator delete(const_cast<void*>(target_to_dealloc));
+	::operator delete(mem - sizeof(size_t));
 	
 }
